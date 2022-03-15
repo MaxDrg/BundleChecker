750517386e8826ba3d6c4bc8d3e9b1b7b726cf80
MaxDrg <maximadda2001@gmail.com>
2022-03-16 00:18:11 +0100

    BundleChecker 2.0.5

--- bot/database.py
+++ bot/database.py
@@ -7,22 +7,18 @@ cfg = Config()
 
 class Database: 
 	def __init__(self):
-		try:
-			ssh = paramiko.SSHClient()
-			ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-			ssh.connect(cfg.host, port=cfg.port, username=cfg.username, password=cfg.passw)
-
-			print("A connection to the server has been established")
-			self.conn = psycopg2.connect(
-  			database = cfg.database,
-			user = cfg.user_data,
-			password = cfg.password_data,
-			host = cfg.host_data,
-			port = cfg.port_data
-			)
-			print ("Database connection established")
-		except Exception as err:
-			print(str(err))
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		ssh.connect(cfg.host, port=cfg.port, username=cfg.username, password=cfg.passw)
+		print("A connection to the server has been established")
+		self.conn = psycopg2.connect(
+  		database = cfg.database,
+		user = cfg.user_data,
+		password = cfg.password_data,
+		host = cfg.host_data,
+		port = cfg.port_data
+		)
+		print ("Database connection established")
 
 	async def add_app(self, appBundle: str, status: str, folder_id: int, startTime: str, nextTime: str):
 		with self.conn.cursor() as cursor:
