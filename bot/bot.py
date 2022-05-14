b653ae120e73f3a0dd7769f3e517a947f562d7bc
Maxim Derhachov <maximderhachov@Maxims-MacBook-Pro.local>
2022-05-15 00:38:04 +0200

    BundleChecker 2.0.3

--- bot/bot.py
+++ bot/bot.py
@@ -329,7 +329,7 @@ async def checkUser(user_id):
     return False
 
 async def scheduler_users():
-    aioschedule.every(1).hours.do(Track.tracking)
+    aioschedule.every().hour.do(Track.tracking)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
